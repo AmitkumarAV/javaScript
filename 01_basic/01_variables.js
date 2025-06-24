b14c4cf749dd1c9f71e97a000c1a0b03ce5a5982
Amit_kumar <kumaramit.nnp@gmail.com>
2025-06-24 11:47:58 +0530

    datatype and variables

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 45456
+let accountemail = "account@example.com"
+var acopuntpassword = "password123"
+accountcity = "New York"
+let accountstate;
+
+
+// accountId = 2  //constant value cannot be changed
+accountemail = "account@example.com"
+acopuntpassword = "password258"
+accountcity= "Los Angeles"
+console.log(accountId);
+console.table([accountId, accountemail, acopuntpassword, accountcity,accountstate])
---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,44 @@
+"use strict"; // Ensures that all variables and functions are defined in strict mode
+// treat all js code as newer versions of JavaScript
+// alert("Hello World!"); // it create problems in older browsers it using in document  and we are using nodejs,not browser
+
+
+console.log(3 + 3); // code readability should be improved/higher
+console.log("Hello World!");
+
+let name = "John Doe";
+let age = 30;
+let isStudent = true;
+
+console.log(`Name: ${name}, Age: ${age}, Is Student: ${isStudent}`);
+
+// number => 2 to power 53
+//bigint
+//string => ""
+// boolean => true or false
+// null
+// undefined
+// symbol => unique identifier
+// object => {}
+  console.log(typeof "Amit"); // string
+  console.log(typeof null); // null is an object in JavaScript
+  console.log(typeof undefined); //undefined
+  console.log(typeof 2); // number
+  console.log(typeof 2n); // bigint
+  console.log(typeof true); // boolean
+  console.log(typeof {}); // object
+  console.log(typeof Symbol("Amit")); // symbol
+  console.log(typeof function() {}); // function
+  console.log(typeof []); // object
+  console.log(typeof new Date()); // object
+  console.log(typeof NaN); // number
+  console.log(typeof Infinity); // number
+  console.log(typeof Symbol.iterator); // symbol
+  console.log(typeof Symbol.prototype); // symbol
+  console.log(typeof Symbol.for("Amit")); // symbol
+  console.log(typeof Symbol.prototype.toString); // function
+  console.log(typeof Symbol.prototype.valueOf); // function
+  console.log(typeof Symbol.prototype.constructor); // function
+
+
+  
