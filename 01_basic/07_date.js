0b380aea4ef9e0eb7e87202fefc05058ee090e8d
Amit_kumar <kumaramit.nnp@gmail.com>
2025-07-04 23:38:31 +0530

    date

---
+++ 01_basic/07_date.js
@@ -0,0 +1,37 @@
+// let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleTimeString())
+// console.log(typeof myDate);// data is an object
+
+// let myCreatedDate = new Date(2025,0,23) // in js month wil be started from 0
+// console.log(myCreatedDate.toDateString()); // it will show date in dd/mm/yyyy format
+
+
+
+// let myCreatedDate2 = new Date(2025,0,23, 5, 6) // in js month wil be started from 0
+// console.log(myCreatedDate2.toLocaleString()); // it will show time in 24 hours format
+
+
+
+
+// let myCreatedDate3 = new Date("01-14-2025") 
+// let mytimestamp = Date.now()
+// console.log(mytimestamp);
+// console.log(myCreatedDate3.getTime());
+// console.log(Date.now()/1000);
+// console.log(Math.floor(Date.now()/1000));
+
+// let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.getMonth() + 1);
+// console.log(myDate.getFullYear());
+// console.log(myDate.getDate());
+// console.log(myDate.getDay()); // it give number
+// console.log(myDate.getHours()); // it give hours
+// console.log(myDate.getMinutes()); // it give minutes
+
+
+
+
